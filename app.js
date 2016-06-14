06278082223172b3f19818dd0345dffe90d4842b
Dylan Holmes <dylanbholmes@gmail.com>
2016-06-14 14:34:59 -0700

    Creation of first script for about me

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 'use strict';
 
-var name = 'Bob';
+var userName = prompt('Hey there! What is your name?');
+alert('Welcome' + ' ' + userName + ',' + ' ' + 'thanks for visiting my site!');
+
+/*
+if (user is correct) {
+    tell them so
+} else {
+    tell them they are wrong
+}
+*/
+
+var answer1 = prompt('How old do you think I am?');
+if (answer1 === 26) {
+  alert('that\'s nearly right! I\'m turning 26 this weekend!');
+} else if (answer1 > 26) {
+  alert('How sweet! Do I actually look old? I always get carded. I am actually 26-ish.');
+} else {
+  alert('Hey I am not THAT young looking. Jeez! I am 26.');
+};
