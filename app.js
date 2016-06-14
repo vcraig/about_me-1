c4440e4e76d185d1e450debddcd4dd7cda2cd3c6
Dylan Holmes <dylanbholmes@gmail.com>
2016-06-14 14:54:09 -0700

    modified y/n prompts

--- app.js
+++ app.js
@@ -11,11 +11,37 @@ if (user is correct) {
 }
 */
 
-var answer1 = prompt('How old do you think I am?');
-if (answer1 === 26) {
-  alert('that\'s nearly right! I\'m turning 26 this weekend!');
-} else if (answer1 > 26) {
-  alert('How sweet! Do I actually look old? I always get carded. I am actually 26-ish.');
+var answer1 = prompt('Do you think Dylan is 26 years old?').toLowerCase();
+if (answer1 === 'yes' || answer1 === 'yep' || answer1 === 'I think so' || answer1 === 'y') {
+  alert('that\'s nearly right! He\'s turning 26 this weekend!');
 } else {
-  alert('Hey I am not THAT young looking. Jeez! I am 26.');
-};
+  alert('Really? he is actually 26-ish.');
+}
+
+var answer2 = prompt('Is Dylan originally from Seattle, Washington?').toLowerCase();
+if (answer2 === 'yes' || answer2 === 'yep' || answer2 === 'I think so' || answer2 === 'y') {
+  alert('Look at you! You\'re right! Wait... did you look Dylan up on Google?');
+} else {
+  alert('Ummmm... well you\'re wrong. He\'s definitely from Seattle.');
+}
+
+var answer3 = prompt('Did Dylan play lacrosse in college?').toLowerCase();
+if (answer3 === 'yes' || answer3 === 'yep' || answer3 === 'I think so' || answer3 === 'y') {
+  alert('Okay I\'m seriously worried you looked Dylan up on Google. That is correct.');
+} else {
+  alert('Ummmm... well you\'re wrong. He definitely played in college... for a bad team. But hey! They got better!');
+}
+
+var answer4 = prompt('Is Dylan\'s favorite dog breed a Bernese Mountain Dog?').toLowerCase();
+if (answer4 === 'yes' || answer4 === 'yep' || answer4 === 'I think so' || answer4 === 'y') {
+  alert('Indubitably! He dreams of frolicking with a pack of Bernese like err\'day.');
+} else {
+  alert('Nope. You\'re wrong. He is all about the Bernese.');
+}
+
+var answer5 = prompt('Is Dylan overly obsessed with Seattle sports teams?').toLowerCase();
+if (answer5 === 'yes' || answer5 === 'yep' || answer5 === 'I think so' || answer5 === 'y') {
+  alert('Sadly yes. It occupies too much of his time and energy.');
+} else {
+  alert('Nope. You\'re wrong. He is all about the Seattle sports teams.');
+}
