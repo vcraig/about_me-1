37ee1f207e080d4dc997f519405e1ed4fa1a2c82
Valerie Craig <valine@gmail.com>
2016-06-16 15:32:46 -0700

    stuff

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # about_me
 About Me page for my Code 201 lab
+
+Dyland and Val - pair programming:
+1. Created two functions for q6 and q7.
--- app.js
+++ app.js
@@ -4,56 +4,56 @@ var userName = prompt('Hey there! What is your name?');
 alert('Welcome ' + userName + ', thanks for visiting my site!');
 
 var correctAns = 0;
-
-var answer1 = prompt('Do you think Dylan is 26 years old?').toLowerCase();
-if (answer1 === 'yes' || answer1 === 'yep' || answer1 === 'i think so' || answer1 === 'y' || answer1 === 'sure' || answer1 === 'i guess') {
-  console.log(userName + ' answered yes to question1 (\'Do you think Dylan is 26 years old?\')');
-  alert('that\'s nearly right! He\'s turning 26 this weekend!');
-  correctAns++;
-} else {
-  console.log(userName + ' did not answer yes to question1 (\'Do you think Dylan is 26 years old?\'). His answer was: ' + answer1);
-  alert('Really? he is actually 26-ish.');
-}
-
-var answer2 = prompt('Is Dylan originally from Seattle, Washington?').toLowerCase();
-if (answer2 === 'yes' || answer2 === 'yep' || answer2 === 'i think so' || answer2 === 'y' || answer2 === 'sure' || answer2 === 'i guess') {
-  console.log(userName + ' answered yes to question2 (\'Is Dylan originally from Seattle, Washington?\')');
-  alert('Look at you! You\'re right! Wait... did you look Dylan up on Google?');
-  correctAns++;
-} else {
-  console.log(userName + ' did not answer yes to question2 (\'Is Dylan originally from Seattle, Washington?\'). His answer was: ' + answer2);
-  alert('Ummmm... well you\'re wrong. He\'s definitely from Seattle.');
-}
-
-var answer3 = prompt('Did Dylan play lacrosse in college?').toLowerCase();
-if (answer3 === 'yes' || answer3 === 'yep' || answer3 === 'i think so' || answer3 === 'y' || answer3 === 'sure' || answer3 === 'i guess') {
-  console.log(userName + ' answered yes to question3 (\'Did Dylan play lacrosse in college?\')');
-  alert('Okay I\'m seriously worried you looked Dylan up on Google. That is correct.');
-  correctAns++;
-} else {
-  console.log(userName + ' did not answer yes to question3 (\'Did Dylan play lacrosse in college?\'). His answer was: ' + answer3);
-  alert('Ummmm... well you\'re wrong. He definitely played in college... for a bad team. But hey! They got better!');
-}
-
-var answer4 = prompt('Is Dylan\'s favorite dog breed a Bernese Mountain Dog?').toLowerCase();
-if (answer4 === 'yes' || answer4 === 'yep' || answer4 === 'i think so' || answer4 === 'y' || answer4 === 'sure' || answer4 === 'i guess') {
-  console.log(userName + ' answered yes to question4 (\'Is Dylan\'s favorite dog breed a Bernese Mountain Dog?\')');
-  alert('Indubitably! He dreams of frolicking with a pack of Bernese like err\'day.');
-  correctAns++;
-} else {
-  console.log(userName + ' did not answer yes to question4 (\'Is Dylan\'s favorite dog breed a Bernese Mountain Dog?\'). His answer was: ' + answer4);
-  alert('Nope. You\'re wrong. He is all about the Bernese.');
-}
-
-var answer5 = prompt('Is Dylan overly obsessed with Seattle sports teams?').toLowerCase();
-if (answer5 === 'yes' || answer5 === 'yep' || answer5 === 'i think so' || answer5 === 'y' || answer5 === 'sure' || answer5 === 'i guess') {
-  console.log(userName + ' answered yes to question5 (\'Is Dylan overly obsessed with Seattle sports teams?\')');
-  alert('Sadly yes. It occupies too much of his time and energy.');
-  correctAns++;
-} else {
-  console.log(userName + ' did not answer yes to question5 (\'Is Dylan overly obsessed with Seattle sports teams?\'). His answer was: ' + answer5);
-  alert('Nope. You\'re wrong. He is all about the Seattle sports teams.');
-}
+//
+// var answer1 = prompt('Do you think Dylan is 26 years old?').toLowerCase();
+// if (answer1 === 'yes' || answer1 === 'yep' || answer1 === 'i think so' || answer1 === 'y' || answer1 === 'sure' || answer1 === 'i guess') {
+//   console.log(userName + ' answered yes to question1 (\'Do you think Dylan is 26 years old?\')');
+//   alert('that\'s nearly right! He\'s turning 26 this weekend!');
+//   correctAns++;
+// } else {
+//   console.log(userName + ' did not answer yes to question1 (\'Do you think Dylan is 26 years old?\'). His answer was: ' + answer1);
+//   alert('Really? he is actually 26-ish.');
+// }
+//
+// var answer2 = prompt('Is Dylan originally from Seattle, Washington?').toLowerCase();
+// if (answer2 === 'yes' || answer2 === 'yep' || answer2 === 'i think so' || answer2 === 'y' || answer2 === 'sure' || answer2 === 'i guess') {
+//   console.log(userName + ' answered yes to question2 (\'Is Dylan originally from Seattle, Washington?\')');
+//   alert('Look at you! You\'re right! Wait... did you look Dylan up on Google?');
+//   correctAns++;
+// } else {
+//   console.log(userName + ' did not answer yes to question2 (\'Is Dylan originally from Seattle, Washington?\'). His answer was: ' + answer2);
+//   alert('Ummmm... well you\'re wrong. He\'s definitely from Seattle.');
+// }
+//
+// var answer3 = prompt('Did Dylan play lacrosse in college?').toLowerCase();
+// if (answer3 === 'yes' || answer3 === 'yep' || answer3 === 'i think so' || answer3 === 'y' || answer3 === 'sure' || answer3 === 'i guess') {
+//   console.log(userName + ' answered yes to question3 (\'Did Dylan play lacrosse in college?\')');
+//   alert('Okay I\'m seriously worried you looked Dylan up on Google. That is correct.');
+//   correctAns++;
+// } else {
+//   console.log(userName + ' did not answer yes to question3 (\'Did Dylan play lacrosse in college?\'). His answer was: ' + answer3);
+//   alert('Ummmm... well you\'re wrong. He definitely played in college... for a bad team. But hey! They got better!');
+// }
+//
+// var answer4 = prompt('Is Dylan\'s favorite dog breed a Bernese Mountain Dog?').toLowerCase();
+// if (answer4 === 'yes' || answer4 === 'yep' || answer4 === 'i think so' || answer4 === 'y' || answer4 === 'sure' || answer4 === 'i guess') {
+//   console.log(userName + ' answered yes to question4 (\'Is Dylan\'s favorite dog breed a Bernese Mountain Dog?\')');
+//   alert('Indubitably! He dreams of frolicking with a pack of Bernese like err\'day.');
+//   correctAns++;
+// } else {
+//   console.log(userName + ' did not answer yes to question4 (\'Is Dylan\'s favorite dog breed a Bernese Mountain Dog?\'). His answer was: ' + answer4);
+//   alert('Nope. You\'re wrong. He is all about the Bernese.');
+// }
+//
+// var answer5 = prompt('Is Dylan overly obsessed with Seattle sports teams?').toLowerCase();
+// if (answer5 === 'yes' || answer5 === 'yep' || answer5 === 'i think so' || answer5 === 'y' || answer5 === 'sure' || answer5 === 'i guess') {
+//   console.log(userName + ' answered yes to question5 (\'Is Dylan overly obsessed with Seattle sports teams?\')');
+//   alert('Sadly yes. It occupies too much of his time and energy.');
+//   correctAns++;
+// } else {
+//   console.log(userName + ' did not answer yes to question5 (\'Is Dylan overly obsessed with Seattle sports teams?\'). His answer was: ' + answer5);
+//   alert('Nope. You\'re wrong. He is all about the Seattle sports teams.');
+// }
 /*
 Also, can make this much more elegant than simply using break;
 Instead, set a variable like 'matchesAnswer6 = false;'
@@ -61,17 +61,18 @@ then in the while use while(matchesAnswer6 === false && counterAnswer6 < 4)
 Add a second if statement outside of the for loop that acts as a catch-all for any incorrect answer
   - if you added this into the inside of the for statement, the negative alert would run every single time a single item in the array is read
 */
-var counterAnswer6 = 0;
-while(counterAnswer6 < 4) {
-  var Answer6 = parseInt(prompt('How many states has Dylan lived in, including Washington State? You only get 4 tries.'));
-  if (Answer6 === 2) {
-    console.log('correct');
-    alert('That\'s spot on!');
-    correctAns++;
-    break;
-  }
-  // can move counterAnswer6++ to outside } on line 82
-  else if (Answer6 < 2) {
+function runLogic() {
+  var counterAnswer6 = 0;
+  while(counterAnswer6 < 4) {
+    var Answer6 = parseInt(prompt('How many states has Dylan lived in, including Washington State? You only get 4   tries.'));
+        if (Answer6 === 2) {
+      console.log('correct');
+      alert('That\'s spot on!');
+      correctAns++;
+      break;
+    }
+    // can move counterAnswer6++ to outside } on line 82
+    else if (Answer6 < 2) {
     console.log('visitor response to Question 6 is ' + Answer6 + ': incorrect');
     alert('That\'s too low, try again!');
     counterAnswer6++;
@@ -87,7 +88,8 @@ while(counterAnswer6 < 4) {
     counterAnswer6++;
   }
 }
-
+}
+runLogic();
 alert('Alrighty, onto the next (and final) question. Here we go!');
 
 /*
@@ -104,21 +106,22 @@ then in the while use while(matchesAnswer7 === false && counterAnswer7 < 7)
 Add a second if statement outside of the for loop that acts as a catch-all for any incorrect answer
   - if you added this into the inside of the for statement, the negative alert would run every single time a single item in the array is read
 */
-var counterAnswer7 = 0;
-while(counterAnswer7 < 7) {
-  var Answer7 = prompt('Which state did Dylan live in besides Washington State? You have a total of 7 tries.').toLowerCase();
-  if (Answer7 === 'ohio' || Answer7 === 'oh') {
-    console.log('question 7 is correct');
-    alert('That\'s spot on! And creepy. Stop looking Dylan up online!');
-    correctAns++;
-    break;
-  }
+function runLogic7() {
+  var counterAnswer7 = 0;
+  while(counterAnswer7 < 7) {
+    var Answer7 = prompt('Which state did Dylan live in besides Washington State? You have a total of 7 tries.').toLowerCase();
+    if (Answer7 === 'ohio' || Answer7 === 'oh') {
+      console.log('question 7 is correct');
+      alert('That\'s spot on! And creepy. Stop looking Dylan up online!');
+      correctAns++;
+      break;
+    }
   // can move counterAnswer7++ to outside } on line 111
-  else if (Answer7 === 'washington' || Answer7 === 'washington state' || Answer7 === 'wa') {
-    console.log('visitor response to Question 7 is ' + Answer7 + ': not valid response');
-    alert('You aren\'t reading the questions, are you?');
-    counterAnswer7++;
-  }
+    else if (Answer7 === 'washington' || Answer7 === 'washington state' || Answer7 === 'wa') {
+      console.log('visitor response to Question 7 is ' + Answer7 + ': not valid response');
+      alert('You aren\'t reading the questions, are you?');
+      counterAnswer7++;
+    }
   else if (isNaN(Answer7) === false) {
     console.log('visitor response to Question 7 is ' + Answer7 + ', which is a number and therefore invalid.');
     alert('Yo! Read the question... CAREFULLY... and try again!');
@@ -130,6 +133,7 @@ while(counterAnswer7 < 7) {
     counterAnswer7++;
   }
 }
-
+}
+runLogic7();
 alert('You got ' + correctAns + ' out of 7 questions correct, ' + userName + '! Better luck next time.');
 console.log(userName + ' recorded ' + correctAns + ' out of 7 questions correct.');
